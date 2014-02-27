69612d7e6124436791be7a5ce99fe34d46638803
Laura McCormack <laura.mccormack227@gmail.com>
2014-02-27 11:57:21 +0000

    add some javascript functionality on the chat section

--- public/main.js
+++ public/main.js
@@ -2,3 +2,24 @@ function hidePoll() {
 	var graph = document.GetElementById("poll");
 
 }
+
+function sendMessage() {
+	var chatMessage = document.getElementById("chat-entry");
+	var chatList = document.getElementById("chat");
+	
+	var chatComment = document.createElement('div');
+    chatComment.class = "chat-comment";
+    
+    var userName = document.createElement('span');
+    userName.class = "chat-username";
+    
+    var userComment = document.createElement('span');
+    userComment.class = "chat-text";
+    
+    userName.innerText = "Tom : ";
+    userComment.innerText = chatMessage.value;
+    
+    chatComment.appendChild(userName);
+    chatComment.appendChild(userComment);
+    chatList.appendChild(chatComment);
+}
