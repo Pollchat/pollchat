c499176fcb6abba17664ddb982aa728c8417380e
Tom Townsend <tom.towny@gmail.com>
2014-02-27 22:05:55 +0000

    Fixes for layout and function of chat and links

--- public/main.js
+++ public/main.js
@@ -5,21 +5,42 @@ function hidePoll() {
 
 function sendMessage() {
 	var chatMessage = document.getElementById("chat-entry");
+    if (chatMessage.value == ""){
+        return
+    }
 	var chatList = document.getElementById("chat");
 	
 	var chatComment = document.createElement('div');
-    chatComment.class = "chat-comment";
+    chatComment.className = "chat-comment";
     
     var userName = document.createElement('span');
-    userName.class = "chat-username";
+    userName.className = "chat-username";
     
     var userComment = document.createElement('span');
-    userComment.class = "chat-text";
+    userComment.className = "chat-text";
     
     userName.innerText = "Tom : ";
     userComment.innerText = chatMessage.value;
     
+    reOrderChat();
+
     chatComment.appendChild(userName);
     chatComment.appendChild(userComment);
     chatList.appendChild(chatComment);
+
+    chatMessage.value = "";
+
+    reOrderChat();
+}
+
+function reOrderChat() {
+    var chatList = document.getElementById("chat");
+    var divs = chatList.children, i = divs.length - 1;
+    for (; i--;){
+        chatList.appendChild(divs[i]);
+    }
+}
+
+function createPoll() {
+    
 }
